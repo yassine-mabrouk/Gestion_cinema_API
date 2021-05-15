1d716e43f4788c688a29bb3441b4a99ce45181f2
Yassine Mabrouk <mabroukyassine70@gmail.com>
2021-05-15 01:55:11 +0000

    first commit

---
+++ src/test/java/enset/com/gc/GestionCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.com.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
